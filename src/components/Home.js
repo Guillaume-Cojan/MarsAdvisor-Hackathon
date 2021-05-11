1065d14ea873cccce0829860c21b96fd5f2d3f46
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-05-11 17:51:25 +0200

    create button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
 import Countdown from "./Countdown";
+import { Switch, Route, Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div>
-            <p>Home component</p>
-            <Countdown />
-        </div>
-    );
+  return (
+    <div>
+      <Link to="/planet">
+        <button>Explore Mars</button>
+      </Link>
+      <Link to ="/planet">
+      <button>Explore the Moon</button>
+      </Link>
+      <Link to ="/planet">
+
+      <button> Explore the Galaxy</button>
+     
+
+      </Link >
+      <Countdown />
+    </div>
+  );
 }
 
 export default Home;
