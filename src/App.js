b58e91d5bc94379627311a162287cc8685ca179d
Francesco <france.luciani@gmail.com>
2021-05-11 18:10:10 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import Image from "./components/Image";
 import Favorites from "./components/Favorites";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCheckSquare);
 
 function App() {
   const [images, setImages] = useState([]);
 
-  const getData = () => {
-    fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=FluwJbwclx3iw8xluHvmGVHaMHi3c8oTYbOYkjDh&start_date=2021-02-10&end_date=2021-05-10"
-    )
-      .then((response) => response.json())
-      .then((data) => setImages(data));
-  };
+  // const getData = () => {
+  //   fetch(
+  //     "https://api.nasa.gov/planetary/apod?api_key=FluwJbwclx3iw8xluHvmGVHaMHi3c8oTYbOYkjDh&start_date=2021-02-10&end_date=2021-05-10"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => setImages(data));
+  // };
 
-  useEffect(getData, []);
+  // useEffect(getData, []);
 
-  console.log(images);
+  // console.log(images);
   return (
     <div className="App">
       <Navbar />
