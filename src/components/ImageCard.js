092b35fe8a421565d2a0a0722435d90842317f20
SilviaCabello <silviacabellom@gmail.com>
2021-05-11 15:50:11 +0200

    pass props to imagelist and imagecard

---
+++ src/components/ImageCard.js
@@ -0,0 +1,9 @@
+function ImageCard( { url }) {
+    return (
+        <div>
+            <img src={url} alt="planet image" />
+        </div>
+    )
+}
+
+export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,13 @@
-function ImageList() {
-    return <p>Image List component</p> 
+import ImageCard from "./ImageCard";
+
+function ImageList({ images }) {
+  return (
+    <div>
+      {images.map((image) => (
+        <ImageCard key={image.date} url={image.url} />
+      ))}
+    </div>
+  );
 }
 
 export default ImageList;
